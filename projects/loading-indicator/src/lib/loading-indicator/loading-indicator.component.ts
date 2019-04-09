69bd8da84e140ab4d96a9015d87f9feed24f81eb
Balázs Tápai <balazs.tapai1990@gmail.com>
2019-04-09 19:28:16 +0200

    trap focus when loading-indicator is present

--- projects/loading-indicator/src/lib/loading-indicator/loading-indicator.component.ts
+++ projects/loading-indicator/src/lib/loading-indicator/loading-indicator.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
 import {isLoading$} from '../loading-indicator.decorators';
 import {LOADING_INDICATOR_CONFIG} from '../loading-indicator.config';
 import {LoadingIndicatorConfig} from '../interfaces/loading-indicator.interfaces';
@@ -9,7 +9,11 @@ import {LoadingIndicatorConfig} from '../interfaces/loading-indicator.interfaces
   templateUrl: './loading-indicator.component.html',
   styleUrls: ['./loading-indicator.component.css']
 })
-export class LoadingIndicatorComponent {
+export class LoadingIndicatorComponent implements OnInit, OnDestroy {
+  @ViewChild('focusTrap')
+  focusTrap: ElementRef;
+  private isLoadingSub: Subscription;
+  private previousFocusTarget: HTMLElement;
 
   constructor(@Inject(LOADING_INDICATOR_CONFIG)
               private config: LoadingIndicatorConfig) {
@@ -23,9 +27,36 @@ export class LoadingIndicatorComponent {
     return `${this.config.size}px`;
   }
 
-  preventClicks(event: UIEvent): void {
-    console.log(event);
+  ngOnInit(): void {
+    this.isLoadingSub = this.isLoading$
+      .subscribe((isLoading: boolean) => {
+        if (isLoading) {
+          this.trapFocus();
+        } else {
+          this.restoreFocus();
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.isLoadingSub.unsubscribe();
+  }
+
+  disableKeyboardEvents(event: KeyboardEvent): void {
     event.preventDefault();
-    event.stopPropagation();
+  }
+
+  private trapFocus(): void {
+    this.previousFocusTarget = document.activeElement as HTMLElement;
+    if (this.previousFocusTarget) {
+      this.previousFocusTarget.blur();
+    }
+    this.focusTrap.nativeElement.focus();
+  }
+
+  private restoreFocus(): void {
+    if (this.previousFocusTarget) {
+      this.previousFocusTarget.focus();
+    }
   }
 }
