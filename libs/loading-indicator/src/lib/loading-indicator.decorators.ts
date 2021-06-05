78e387e3fdb1b9b833d75259e46a927ce18e3ea7
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-06-05 11:30:09 +0200

    feat: queue operator

--- libs/loading-indicator/src/lib/loading-indicator.decorators.ts
+++ libs/loading-indicator/src/lib/loading-indicator.decorators.ts
@@ -1,9 +1,60 @@
-import {BehaviorSubject, Observable} from 'rxjs';
-import {distinctUntilChanged, tap} from 'rxjs/operators';
+import {BehaviorSubject, Observable, Subscriber} from 'rxjs';
+import {filter, map, pluck, share, tap} from 'rxjs/operators';
 
 const indicatorSubject = new BehaviorSubject<boolean>(false);
 
-export const isLoading$ = indicatorSubject.asObservable().pipe(distinctUntilChanged());
+const indicatorState$ = indicatorSubject.asObservable().pipe(share());
+
+const indicatorStart$ = indicatorState$.pipe(filter(indicatorState => indicatorState));
+const indicatorStop$ = indicatorState$.pipe(filter(indicatorState => !indicatorState));
+
+export const isLoading$ = indicatorStart$.pipe(
+  fifoQueue(indicatorStop$, {emitQueue: true}),
+  pluck('length'),
+  map(Boolean),
+);
+
+function fifoQueue<T>(emit$: Observable<T | T[]>, config: { emitQueue: boolean } = {emitQueue: false}): (source$: Observable<T>) => Observable<T | T[]> {
+  const values: T[] = [];
+
+  return (source$: Observable<T>) => {
+    return new Observable<T | T[]>((observer: Subscriber<unknown>) => {
+      const sourceSub = source$
+        .subscribe({
+          next: (value) => {
+            values.push(value);
+            if (config.emitQueue) {
+              observer.next(values);
+            }
+          },
+          error: observer.error,
+          complete: observer.complete
+        });
+
+      const emitterSub = emit$
+        .subscribe(({
+          next: () => {
+            const value = values.shift();
+            if (config.emitQueue) {
+              observer.next(values);
+            } else {
+              observer.next(value);
+            }
+          },
+          error: observer.error,
+          complete: observer.complete
+        }));
+
+      return {
+        unsubscribe() {
+          sourceSub.unsubscribe();
+          emitterSub.unsubscribe();
+        }
+      };
+    });
+  };
+}
+
 
 export function startLoadingIndicator(): (target: any, propertyKey: string | symbol, propertyDescriptor: PropertyDescriptor) => any {
   return (target: any, propertyKey: string | symbol, propertyDescriptor: PropertyDescriptor): any => {
