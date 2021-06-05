79a3845a3db8f1ce33a4149e0727a4b039fe8965
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-06-05 10:13:27 +0200

    feat: added loading indicator bug reproduction

--- apps/ng-reusables/src/app/loading-indicator-showcase/loading-indicator-showcase.component.ts
+++ apps/ng-reusables/src/app/loading-indicator-showcase/loading-indicator-showcase.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { startLoadingIndicator, stopLoadingIndicator } from '@btapai/ng-loading-indicator';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-loading-indicator-showcase',
@@ -7,13 +8,35 @@ import { startLoadingIndicator, stopLoadingIndicator } from '@btapai/ng-loading-
   styleUrls: ['./loading-indicator-showcase.component.css']
 })
 export class LoadingIndicatorShowcaseComponent {
+
+  readonly control = new FormControl(1000)
+  readonly control1 = new FormControl(1000)
+  readonly control2 = new FormControl(1000)
+  readonly control3 = new FormControl(1000)
+
+  multiTrigger = [10000, 5000];
+
   @startLoadingIndicator()
-  triggerLoadingIndicator() {
-    setTimeout(this.triggerLoadingIndicatorStop.bind(this), 100);
+  triggerLoadingIndicator(customValue?: number): void {
+    setTimeout(this.triggerLoadingIndicatorStop.bind(this), customValue || this.control.value);
+  }
+
+  addToMultiTrigger(value: string) {
+    this.multiTrigger.push(parseInt(value, 10));
+  }
+
+  triggerMultipleLoadingIndicators(): void {
+    this.multiTrigger.forEach((timeout) => {
+      this.triggerLoadingIndicator(timeout);
+    })
+  }
+
+  clearMultiTrigger(): void {
+    this.multiTrigger = [];
   }
 
   @stopLoadingIndicator()
-  triggerLoadingIndicatorStop() {
+  triggerLoadingIndicatorStop(): void {
     console.log('stopped');
   }
 }
